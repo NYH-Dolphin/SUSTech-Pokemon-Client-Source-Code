86971e475df5184b2fc48af7dc8a7ec11f8fbfa4
小小的大肥 <11911839@mail.sustech.edu.cn>
2021-10-06 16:11:02 +0800

    Finish part of pokemon and user's movement and level design

---
+++ Assets/Script/Pokemon/PokemonMove.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PokemonMove : MonoBehaviour
+{
+    private float speed = 3.0f;
+
+    private Vector2 originalPos; // 宝可梦的初始位置
+    public Animator anima;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = transform.position;
+    }
+
+    // Update is called once per frame
+
+    private float interval = 0.0f;
+    private string state = "Move";
+
+    void Update()
+    {
+        if (interval >= 1f)
+        {
+            interval = 0.0f;
+            if (Random.Range(0f, 1f) < 0.2)
+            {
+                state = "Stop";
+            }
+            else
+            {
+                state = "Move";
+            }
+        }
+        else
+        {
+            interval += Time.deltaTime;
+        }
+
+        switch (state)
+        {
+            case "Move":
+                MoveHorizontally(14f);
+                break;
+            case "Stop":
+                Stop();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 水平移动的宝可梦
+    /// </summary>
+    /// <param name="distance"></param> 水平移动的范围距离初始位置的距离的绝对值
+    private string stateMove = "Left";
+
+    private static readonly int IsLeft = Animator.StringToHash("isLeft");
+
+    void MoveHorizontally(float distance)
+    {
+        switch (stateMove)
+        {
+            case "Right":
+                if (transform.position.x - originalPos.x >= 0 && transform.position.x - originalPos.x <= distance)
+                {
+                    anima.SetBool(IsLeft, false);
+                    transform.Translate(new Vector3(1, originalPos.y, 0) * Time.deltaTime * speed);
+                }
+                else if (transform.position.x - originalPos.x > distance)
+                {
+                    transform.position = new Vector3(originalPos.x + distance, originalPos.y);
+                    stateMove = "Left";
+                }
+
+                break;
+            case "Left":
+                if (transform.position.x - originalPos.x >= 0 && transform.position.x - originalPos.x <= distance)
+                {
+                    anima.SetBool(IsLeft, true);
+                    transform.Translate(new Vector3(-1, originalPos.y, 0) * Time.deltaTime * speed);
+                }
+                else if (transform.position.x - originalPos.x <= 0)
+                {
+                    transform.position = originalPos;
+                    stateMove = "Right";
+                }
+
+                break;
+        }
+    }
+
+
+    void Stop()
+    {
+        Debug.Log("Hello");
+    }
+}
--- Assets/Script/User/UserControl.cs
+++ Assets/Script/User/UserControl.cs
@@ -6,6 +6,13 @@ using UnityEngine;
 public class UserControl : MonoBehaviour
 {
     private float speed = 8.0f;
+    public Animator anima; // Animator 组件
+    private int direction = 0; // 0-右边，1-左边，2-上边 3-下边
+    private static readonly int Left = Animator.StringToHash("left");
+    private static readonly int Right = Animator.StringToHash("right");
+    private static readonly int Up = Animator.StringToHash("up");
+    private static readonly int Down = Animator.StringToHash("down");
+    private static readonly int Speed = Animator.StringToHash("speed");
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +22,92 @@ public class UserControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void FixedUpdate()
     {
         // 人物移动
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
-        transform.Translate(new Vector3(horizontal, vertical, 0) * Time.deltaTime * speed);
+        float moveY = Input.GetAxis("Vertical");
+        float moveX = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(moveX, moveY, 0) * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction = 0;
+            anima.SetBool(Left, false);
+            anima.SetBool(Right, true);
+            anima.SetBool(Up, false);
+            anima.SetBool(Down, false);
+            anima.SetFloat(Speed, speed);
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction = 1;
+            anima.SetBool(Left, true);
+            anima.SetBool(Right, false);
+            anima.SetBool(Up, false);
+            anima.SetBool(Down, false);
+            anima.SetFloat(Speed, speed);
+        }
+       
+       if (Input.GetKey(KeyCode.W))
+        {
+            direction = 2;
+            anima.SetBool(Left, false);
+            anima.SetBool(Right, false);
+            anima.SetBool(Up, true);
+            anima.SetBool(Down, false);
+            anima.SetFloat(Speed, speed);
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction = 3;
+            anima.SetBool(Left, false);
+            anima.SetBool(Right, false);
+            anima.SetBool(Up, false);
+            anima.SetBool(Down, true);
+            anima.SetFloat(Speed, speed);
+        }
+        
+        if (!Input.anyKey)
+        {
+            switch (direction)
+            {
+                case 0:
+                {
+                    anima.SetBool(Left, false);
+                    anima.SetBool(Right, true);
+                    anima.SetBool(Up, false);
+                    anima.SetBool(Down, false);
+                    break;
+                }
+                case 1:
+                {
+                    anima.SetBool(Left, true);
+                    anima.SetBool(Right, false);
+                    anima.SetBool(Up, false);
+                    anima.SetBool(Down, false);
+                    break;
+                }
+                case 2:
+                {
+                    anima.SetBool(Left, false);
+                    anima.SetBool(Right, false);
+                    anima.SetBool(Up, true);
+                    anima.SetBool(Down, false);
+                    break;
+                }
+                case 3:
+                {
+                    anima.SetBool(Left, false);
+                    anima.SetBool(Right, false);
+                    anima.SetBool(Up, false);
+                    anima.SetBool(Down, true);
+                    break;
+                }
+            }
+            anima.SetFloat(Speed, 0.0f);
+        }
     }
-    
 }
