d9dd76e868f5f7712b480dc3ffba2551a9678545
小小的大肥 <11911839@mail.sustech.edu.cn>
2021-11-13 22:42:57 +0800

    Part of pokemon logic

--- Assets/Script/UI/PokemonManager.cs
+++ Assets/Script/UI/PokemonManager.cs
@@ -7,13 +7,16 @@ using UnityEngine.UI;
 
 public class PokemonManager : MonoBehaviour
 {
-    public List<Toggle> ToggleList; // 四个toggle
-
     public List<Canvas> CanvasList;
+    public List<Toggle> GridList;
+
+    private int pageNum;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        UserDataSync();
         // 先把第0个Canvas设置显示
         CanvasList[0].enabled = true;
         for (int i = 1; i < CanvasList.Count; i++)
@@ -25,12 +28,43 @@ public class PokemonManager : MonoBehaviour
     {
     }
 
+
+    public void OnClickNextPage()
+    {
+        if (pageNum < User.GetInstance().Pokemons.Count / 9)
+            pageNum++;
+
+        UserDataSync();
+    }
+
+    public void OnClickPrevPage()
+    {
+        pageNum = pageNum > 0 ? pageNum - 1 : pageNum;
+        UserDataSync();
+    }
+
+    private void UserDataSync()
+    {
+        int index = 9 * pageNum;
+        User user = User.GetInstance();
+        foreach (Toggle grid in GridList)
+        {
+            Image pokemonImg = grid.transform.GetChild(0).transform.GetChild(0).GetComponent<Image>();
+            string path = index < user.Pokemons.Count
+                ? "Pokemon/Portrait/" + user.Pokemons[index].ID
+                : "Pokemon/Portrait/0";
+            Sprite sprite = Resources.Load(path, typeof(Sprite)) as Sprite;
+            pokemonImg.sprite = sprite;
+            index++;
+        }
+    }
+
     public void BackToMainScene()
     {
         SceneManager.LoadScene("Main");
     }
 
-    
+
     // 切换某个Canvas到enable状态
     private void EnableCanvas(int num)
     {
