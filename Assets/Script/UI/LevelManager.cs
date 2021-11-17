14e67115efd82bf51f6825d82586f7c3ca96ad94
小小的大肥 <11911839@mail.sustech.edu.cn>
2021-11-18 00:05:58 +0800

    Part of adventure

--- Assets/Script/UI/LevelManager.cs
+++ Assets/Script/UI/LevelManager.cs
@@ -9,7 +9,10 @@ public class LevelManager : MonoBehaviour
 
     public Text UserInfo;
     public Image Portrait;
-    
+    public Image pokemon1;
+    public Image pokemon2;
+    public Image pokemon3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,8 +32,19 @@ public class LevelManager : MonoBehaviour
         string path = "User/Portrait/p" + user.Portrait;
         Sprite sprite = Resources.Load(path, typeof(Sprite)) as Sprite;
         Portrait.sprite = sprite;
+        ChangeDisplayPokemon(pokemon1, user.AdventurePokemon1.ID);
+        ChangeDisplayPokemon(pokemon2, user.AdventurePokemon2.ID);
+        ChangeDisplayPokemon(pokemon3, user.AdventurePokemon3.ID);
     }
 
+    public void ChangeDisplayPokemon(Image pokemon, int num)
+    {
+        string path = "Pokemon/Portrait/" + num;
+        Sprite sprite = Resources.Load(path, typeof(Sprite)) as Sprite;
+        pokemon.sprite = sprite;
+        pokemon.SetNativeSize();
+        pokemon.GetComponent<RectTransform>().localScale = new Vector3(0.6f, 0.6f, 0.6f);
+    }
 
     public void BackToTheAdventureScene()
     {
